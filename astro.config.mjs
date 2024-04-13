5638f3929fbcc1d33a084406d700641e75ad8669
dinwwwh <dinwwwh@gmail.com>
2024-04-13 15:21:35 +0700

    chore: layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   srcDir: '.',
+  site: 'https://dinwwwh.com',
   integrations: [tailwind()],
 })
