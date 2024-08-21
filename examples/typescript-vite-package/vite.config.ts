2241537617bd6b16209f42de659c62a38f9f6956
dinwwwh <dinwwwh@gmail.com>
2024-08-21 20:13:36 +0700

    feat(example): typescript-vite-package

---
+++ examples/typescript-vite-package/vite.config.ts
@@ -0,0 +1,28 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  ssr: {
+    noExternal: [], // Packages that should be bundled
+  },
+  build: {
+    ssr: true, // Prevent bundle all dependencies (except linked dependencies, and above noExternal list) and make it usable in node.js
+    target: 'es2022',
+    lib: {
+      entry: [path.resolve(__dirname, './src/index.ts')],
+      formats: ['es'],
+    },
+  },
+  plugins: [
+    tsconfigPaths(),
+    dts({
+      compilerOptions: {
+        baseUrl: '.', // This will help dts resolve alias imports
+        types: [],
+      },
+    }),
+  ],
+})
