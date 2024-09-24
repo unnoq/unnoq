256c9a25bb3ecdb1d39145f96a81ef712724f95c
dinwwwh <dinwwwh@gmail.com>
2024-09-24 08:07:00 +0700

    fix(examples): ssr config

--- examples/typescript-vite-app/vite.config.ts
+++ examples/typescript-vite-app/vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 // @ts-expect-error vite-plugin-on-success is not typed
 import { onSuccess } from 'vite-plugin-on-success'
-import pkg from './package.json'
 
 export default defineConfig({
-  ssr: {
-    external: true,
-    noExternal: Object.keys(pkg.devDependencies),
-  },
   build: {
     ssr: true,
     lib: {
--- examples/typescript-vite-package/vite.config.ts
+++ examples/typescript-vite-package/vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite'
-import pkg from './package.json'
 
 export default defineConfig({
-  ssr: {
-    external: true,
-    noExternal: Object.keys(pkg.devDependencies),
-  },
   build: {
     ssr: true,
     sourcemap: true,
     lib: {
       entry: {
-        'src/index': 'src/index.ts',
+        index: 'src/index.ts',
       },
       formats: ['es'],
     },
