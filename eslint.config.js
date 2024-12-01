662e3754030b64724e97e59bb20ebf414b72a120
unnoq <contact@unnoq.com>
2024-12-01 08:44:13 +0700

    ci: commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,5 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  astro: true,
   formatters: true,
-}, {
-  rules: {
-    'ts/consistent-type-definitions': 'off',
-    'react-refresh/only-export-components': 'off',
-    'react/prefer-destructuring-assignment': 'off',
-  },
 })
