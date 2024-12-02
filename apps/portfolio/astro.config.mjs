08a6de30aa8c80bee5cea9dfaa7b70dfef86638f
unnoq <contact@unnoq.com>
2024-12-02 07:55:05 +0700

    chore: apply lin fixes

--- apps/portfolio/astro.config.mjs
+++ apps/portfolio/astro.config.mjs
@@ -1,7 +1,7 @@
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
 import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
